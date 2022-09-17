3e4f387ee331c9917c897cdf102e61ea5e7648ce
Vicary A <vicary.archangel@member.mensa.org>
2022-09-17 10:25:58 +0800

    feat: export Task interface

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 export type { Executable } from "./Executable.ts";
 export { ExecutableWorker } from "./ExecutableWorker.ts";
 export { Runner, RunnerExecutionError } from "./Runner.ts";
+export type { Task } from "./Task.ts";
 export { Workerpool } from "./Workerpool.ts";
 export type { WorkerpoolOptions } from "./Workerpool.ts";
